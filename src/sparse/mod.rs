647af811f24d342aa6fdcef7cf3b4d251b596fc3
James Logan <jlogan03@gmail.com>
2026-04-08 08:21:03 -0400

    implement lagged LU preconditioner and compensated LU solve

--- src/sparse/mod.rs
+++ src/sparse/mod.rs
@@ -2,11 +2,13 @@ pub mod bicgstab;
 pub(crate) mod col;
 pub mod compensated;
 pub mod equilibration;
+pub mod lu;
 pub mod matvec;
 pub mod precond;
 
 pub use bicgstab::BiCGSTAB;
 pub use compensated::CompensatedField;
 pub use equilibration::{Equilibration, EquilibrationError, EquilibrationParams};
+pub use lu::{LuRefinementParams, RefinedLuSolve, SparseLu, SparseLuError};
 pub use matvec::SparseMatVec;
 pub use precond::{BiPrecond, DiagonalPrecond, DiagonalPrecondError, IdentityPrecond, Precond};
